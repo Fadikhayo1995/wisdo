7cd55d9f86d6248a20fa1125ea074f33028c4f23
Fadi Khayo <khayofadi@gmail.com>
2021-09-28 08:03:21 +0300

    removing multiple spaces

--- Server/Utils/StringUtils.js
+++ Server/Utils/StringUtils.js
@@ -2,6 +2,8 @@ module.exports = {
   getFirstNWords: function (num, value) {
     var wordsCount = 0;
     var newString = '';
+    value = value.replace(/\s\s+/g, ' ');
+
     for (var index = 0; index < value.length; index++){
       if (value[index].match(/[^a-zA-Z0-9,.']/g)) {
         wordsCount++;
